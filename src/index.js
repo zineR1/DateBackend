e03d048e0d6016e33b0532d8946869b27349fb5e
zineR1 <agusdalvit@gmail.com>
2023-11-18 14:38:06 -0300

    false

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import http from "http";
 import app from "./app.js";
 import { sequelize } from "./database/database.js";
-import { Server } from "socket.io";
+// import { Server } from "socket.io";
 import { createRootUser } from './controllers/usersRoot/usersRoot.js';
 import { createEventRoot } from './controllers/eventsRoot/eventsRoot.js'
 import colors from 'colors';
@@ -9,16 +9,16 @@ import colors from 'colors';
 const PORT = process.env.PORT || 3001;
 
 async function main() {
-  await sequelize.sync({ force: true });
+  await sequelize.sync({ force: false });
   const server = http.createServer(app);
-  const io = new Server();
-  io.on("connection", (socket) => {
-    console.log("A user connected");
+  // const io = new Server();
+  // io.on("connection", (socket) => {
+  //   console.log("A user connected");
 
-    socket.on("disconnect", () => {
-      console.log("User disconnected");
-    });
-  });
+  //   socket.on("disconnect", () => {
+  //     console.log("User disconnected");
+  //   });
+  // });
 
   server.listen(PORT, () => {
     console.log(colors.black.bgGreen(`Server running on ${PORT}`))
