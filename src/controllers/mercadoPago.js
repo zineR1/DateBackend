e246e02c533e6479c3aef440362ec9db7afba4b4
zineR1 <agusdalvit@gmail.com>
2024-03-21 23:12:55 -0300

    json

--- src/controllers/mercadoPago.js
+++ src/controllers/mercadoPago.js
@@ -29,7 +29,7 @@ export const createMPToken = async (req, res) => {
       user.mercadoPagoToken = accessTokenResponse.data;
       await user.save();
       console.log(accessTokenResponse, "ACCESTOKEN MP");
-      res.send({ data: user, msj: "Token de mercado pago guardado con éxito" });
+      res.json({ data: user, msj: "Token de mercado pago guardado con éxito" });
     }
   } catch (error) {
     res.send(error);
