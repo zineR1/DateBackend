c9896f556171dd2d6255b35af595d6140adc9a1c
zineR1 <agusdalvit@gmail.com>
2024-03-28 04:52:57 -0300

    actualizacionBackend

--- src/controllers/events.js
+++ src/controllers/events.js
@@ -6,7 +6,7 @@ import { Guest } from "../models/Guest.js";
 import multer from "multer";
 import path from "path";
 
-const urlBackend = process.env.URL_BACKEND;
+const urlBackend = process.env.URL_BACKEND_QA;
 
 export const getEvents = async (req, res) => {
   try {
--- src/controllers/mercadoPago.js
+++ src/controllers/mercadoPago.js
@@ -1,15 +1,12 @@
 import { MercadoPagoConfig, Preference } from "mercadopago";
-// import { User } from "../models/User.js";
 import { Event } from "../models/Event.js";
 import axios from "axios";
-const urlBackend = process.env.URL_BACKEND;
-
+const urlBackend = process.env.URL_BACKEND_QA;
+const urlFrontend = process.env.URL_FRONTEND_QA;
 
 export const createMPToken = async (req, res) => {
   const client_id = process.env.CLIENT_ID_MP;
-const client_secret = process.env.CLIENT_SECRET_MP;
-console.log(client_id,"CLIENT_ID ESTE")
-console.log(client_secret, "CLIENT_SECRET")
+  const client_secret = process.env.CLIENT_SECRET_MP;
   const { code, eventId } = req.body;
   try {
     if (code && client_id && client_secret) {
@@ -18,7 +15,7 @@ console.log(client_secret, "CLIENT_SECRET")
         client_secret: client_secret,
         code: code,
         grant_type: "authorization_code",
-        redirect_uri: "https://datefrontendpruebas.onrender.com/",
+        redirect_uri: `${urlFrontend}/`,
       };
 
       const accessTokenResponse = await axios.post(
@@ -34,16 +31,27 @@ console.log(client_secret, "CLIENT_SECRET")
       event.mercadoPagoToken = accessTokenResponse.data;
       await event.save();
       res.send("Token de mercado pago guardado con éxito");
-    }else{console.log("NO EJECUTÉ LA FUNCIÓN PARA LA COMPRA")}
+    }
   } catch (error) {
     res.send(error);
   }
 };
 
 export const createOrder = async (req, res) => {
-  const { eventId, products } = req.body;
-  const event = await Event.findByPk(2);
-  console.log(event, "eventO BUSCADDO EN CREAR ORADEN");
+  const { eventId, title, quantity, price } = req.body;
+  const calculatePercentage = (number, perc) => {
+    return (number * perc) / 100;
+  };
+  const amount = price * quantity;
+  const vincufyFee = calculatePercentage(amount, 6);
+  const mercadoPagoCost = calculatePercentage(amount, 6.99);
+  const mpCostWithIVA = calculatePercentage(mercadoPagoCost, 21);
+  const totalPrice = amount + vincufyFee + mpCostWithIVA;
+  const fullTitle = `${title} + Costo de servicio`;
+  const products = [
+    { title: fullTitle, quantity: quantity, unit_price: totalPrice },
+  ];
+  const event = await Event.findByPk(eventId);
   const accessToken =
     event && event.mercadoPagoToken && event.mercadoPagoToken.access_token;
   try {
@@ -60,12 +68,12 @@ export const createOrder = async (req, res) => {
         //     excluded_payment_types: [],
         //     installments: 12,
         //   },
-        // items: products,
-        items: [{ title: "Una productazo", quantity: 1, unit_price: 100 }],
+        items: products,
+        // items: [{ title: "Un productazo", quantity: 1, unit_price: 100 }],
         back_urls: {
-          success: `${urlBackend}/success`,
-          failure: `${urlBackend}/failure`,
-          pending: `${urlBackend}/pending`,
+          success: `${urlFrontend}/resultMP/success`,
+          failure: `${urlFrontend}/resultMP/failure`,
+          pending: `${urlFrontend}/resultMP/pending`,
         },
         notification_url: `${urlBackend}/webhook`,
       },
